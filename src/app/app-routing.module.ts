67bd9db3bd72057d8aeec8b45c93e50b35fe4218
marcos <marcos@slice.global>
2023-04-29 17:42:22 -0300

    correção

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegistrosComponent } from './components/registros/registros.component';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 
 const routes: Routes = [
