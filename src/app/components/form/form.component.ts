c230dc2f109cfaf619607803009adc42ca63b455
marcos <marcos@slice.global>
2023-04-30 18:59:32 -0300

    pegando o nome do link

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+
+  nome: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.nome = params['nome'];
+    });
+
+    console.log(this.nome);
+  }
+
+
 
 }
