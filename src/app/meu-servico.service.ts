d95b26d5a1b169340ecdc7e86f5b1efefa4545b2
marcos <marcos@slice.global>
2023-04-30 16:35:15 -0300

    mudando status

--- src/app/meu-servico.service.ts
+++ src/app/meu-servico.service.ts
@@ -20,6 +20,13 @@ export class MeuServicoService {
 
   }
 
+  status(id: any, updatedData: undefined): void {
+    this.http.put(`http://127.0.0.1:8000/api/validar/${id}`, { cpf: '555555' }).subscribe(resultado => {
+    console.log('Recurso atualizado:', resultado);
+  });
+
+  }
+
   setInformacoes(dados: any) {
     sessionStorage.setItem('id', dados);
     // this.localStorageService.set('idade', 30);
