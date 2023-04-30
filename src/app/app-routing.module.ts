4db6de38bb5bb70891316484652e0cd4439fb9ea
marcos <marcos@slice.global>
2023-04-30 14:47:40 -0300

    conserto de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegistrosComponent } from './components/registros/registros.component';
-// import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { DetailsComponent } from './components/registros/details/details.component';
 
@@ -10,7 +9,7 @@ import { DetailsComponent } from './components/registros/details/details.compone
 const routes: Routes = [
   { path: 'registros', component: RegistrosComponent },
   { path: 'form', component: FormComponent },
-  { path: 'detalhes/:nome/validar', component: DetailsComponent },
+  { path: ':nome/validar', component: DetailsComponent },
 ];
 
 @NgModule({
