4ef159480b571115bd46f294e81748b945909c6e
marcos <marcos@slice.global>
2023-05-01 01:02:03 -0300

    validando dados

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -19,29 +19,51 @@ export class FormComponent implements OnInit {
   retorno: any;
   msg: any;
 
-
-  constructor(private route: ActivatedRoute, private meuServico: MeuServicoService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private meuServico: MeuServicoService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.nome = params['nome'];
     });
-
-
   }
 
   onSubmit() {
-
-    let dadosForm = {"nome":this.name, "email":this.email, "cpf":this.cpf, "celular":this.phone, "conhecimentos":this.knowledge}
+    let dadosForm = {
+      nome: this.name,
+      email: this.email,
+      cpf: this.cpf,
+      celular: this.phone,
+      conhecimentos: this.knowledge,
+    };
 
     this.meuServico.submitForm(dadosForm);
-    let form:any = sessionStorage.getItem('form');
-    this.msg = form.split('"');
+    // let form:any = sessionStorage.getItem('form');
+    // sessionStorage.clear();
+    // this.msg = form.split('"');
 
-    console.log(this.msg[3]);
+    // console.log(this.msg[3]);
 
-
+    if (
+      this.name == undefined ||
+      this.email == undefined ||
+      this.cpf == undefined ||
+      this.phone == undefined ||
+      this.knowledge == undefined
+    ) {
+      this.msg = 'Preencha todos os campos';
+    } else {
+      const email: any = this.email;
+      if (validarEmail(email)) {
+      } else {
+        this.msg ='O email é inválido.';
+      }
+      function validarEmail(email: string) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+      }
+    }
   }
-
-
 }
